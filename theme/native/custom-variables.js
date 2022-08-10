326350ea1fd7fa32cca8a5a2ea970dce2c284085
Palanivelrajan P <palanivel.rajan@rapiddatatech.com>
2022-08-10 10:59:20 +0000

    After  CommitRequest Api consume

--- theme/native/custom-variables.js
+++ theme/native/custom-variables.js
@@ -403,6 +403,7 @@ export const tabContainer = {
         width: 100, 
         borderRightWidth:1,
         borderColor: '#000',
+        scrollEnabled: true
     },
     tab: {
         paddingVertical: 12,
